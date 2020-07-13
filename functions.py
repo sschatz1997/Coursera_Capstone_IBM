79c24b6f54e674cc4ef607137b47560842798880
sschatz1997 <s.schatz1997@gmail.com>
2020-07-13 14:29:19 -0400

    data

---
+++ functions.py
@@ -0,0 +1,56 @@
+# functions.py
+# system imports
+import re
+import requests
+
+# data imports
+import pandas as pd
+import wikipedia as wp # this pulls from wikipedia
+
+# time functions
+import time
+import datetime
+from datetime import date
+from time import sleep as s
+from datetime import datetime
+
+def writeBaltimorePopulation():
+    """
+    Table info:
+    Title: Historical population
+    Original headers: [ Census, Pop., Unnamed: 2_level_1, %Â± ]
+    New headers: ["Census", "Pop.", "Percent"]
+    """
+    path = 'E:\\LEARN\\Coursera_Capstone_IBM\\DATA\\'
+    file1 = path + "balt_pop.csv"
+    NH = ["Census", "Population", "Percent"] # new header
+    html = wp.page("Baltimore: Population").html().encode('UTF-8')
+    df = pd.read_html(html)[4] # data location of the table
+    #print(df.head())
+    cen = df['Historical population']['Census'] # census year
+    pop = df['Historical population']['Pop.'] # population
+    perc = df['Historical population']['%Â±'] # percent change
+
+    # NDF = new dataframe
+    NDF = {
+        'Census': cen,
+        'Population': pop,
+        'Percent': perc 
+    }
+
+    # convert to a better dataframe
+    dataOut = pd.DataFrame(NDF, columns = ["Census", "Population", "Percent"])
+
+    # delete the first and last row
+    dataOut = dataOut.drop([24])
+    dataOut = dataOut.drop([0])
+
+    for index, row in dataOut.iterrows():
+        if 'â' in str(row['Percent']):
+            row['Population'].strip('â')
+        if 'Est. ' in str(row['Census']):
+            row['Census'].strip('Est. ')
+
+    dataOut.to_csv(file1, encoding='UTF-8', index=False)
+
+getBaltimorePopulation()
